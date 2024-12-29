d5f2ecf5051a05f108624796c9e750d38bdaae29
edhit <46953444+edhit@users.noreply.github.com>
2024-12-29 06:47:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,21 +34,21 @@ const text = process.env.START
 bot.start(async (ctx) => {
   try {
     if (ctx.chat && ctx.chat.username) {
-      // await ctx.unpinAllChatMessages();
+      await ctx.unpinAllChatMessages();
       const message_data = await ctx.reply(text, {
-        // reply_markup: {
-        //   inline_keyboard: [
-        //     [
-        //       {
-        //         text: "Все объявления",
-        //         url: `https://t.me/${process.env.LINK}`, // Укажите URL вашего WebApp
-        //       },
-        //     ],
-        //   ],
-        // },
+        reply_markup: {
+           inline_keyboard: [
+             [
+               {
+                 text: "Биржа",
+                 url: `https://t.me/${process.env.LINK}`, // Укажите URL вашего WebApp
+               },
+             ],
+           ],
+         },
         disable_web_page_preview: true, // Отключение превью ссылки
       });
-      // await ctx.pinChatMessage(message_data.message_id);
+      await ctx.pinChatMessage(message_data.message_id);
     } else {
       await ctx.reply(
         "✨ Ас-саляму ‘аляйкум ва рахмату-Ллахи ва баракяту\n\n🛂 Чтобы начать использовать бота, пожалуйста, укажите имя пользователя в настройках Telegram. Перейдите в настройки Telegram, откройте раздел 'Изменить профиль' и добавьте ваше имя пользователя."
